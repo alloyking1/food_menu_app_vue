9e3c82fedee1e4d405a7c0db77bc7499bc193f82
alloyking1 <alloyking1@gmail.com>
2023-10-23 15:40:44 +0100

    fixed vuex bug

--- src/components/pages/HomeView.vue
+++ src/components/pages/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
     <div class="bg-green-500 text-white">
-        skjdlfkjsdlksj
+        <pre>
+            {{ meal }}
+        </pre>
     </div>
 </template>
+
+<script setup>
+import { computed } from 'vue';
+import store from '../../store'
+
+
+const meal = computed (() => store.state.meal)
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from 'vuex'
+import state from './state'
+
+const store = createStore({
+    state
+    // actions,
+    // mutations,
+    // getters
+})
+
+export default store
